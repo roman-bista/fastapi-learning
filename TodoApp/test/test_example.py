4eb6a4483b6d2896f123453d56ac1dd3859fea66
Roman <roman@Romans-MacBook-Air.local>
2026-06-14 22:00:23 +0545

    Learn pytest assertions, class testing, and fixtures

--- TodoApp/test/test_example.py
+++ TodoApp/test/test_example.py
@@ -1,34 +1,54 @@
-def test_equall_or_not_equall():
-    assert 3 == 3
-    assert 3!=1
+import pytest
 
-def test_is_instance():
-    # isinstance(object, type)
-    assert isinstance('this is a sttring', str)
-    assert not isinstance('10', int)
+# def test_equall_or_not_equall():
+#     assert 3 == 3
+#     assert 3!=1
+
+# def test_is_instance():
+#     # isinstance(object, type)
+#     assert isinstance('this is a sttring', str)
+#     assert not isinstance('10', int)
+
+# # def test_boolean():
+# #     validated = True
+# #     assert validated is True
+# #     assert ('hello'=='world') is False
 
 # def test_boolean():
 #     validated = True
-#     assert validated is True
-#     assert ('hello'=='world') is False
 
-def test_boolean():
-    validated = True
+#     assert validated
+#     assert 'hello' != 'world'
 
-    assert validated
-    assert 'hello' != 'world'
+# def test_type():
+#     assert type('hello') is str
+#     assert type(10) is int
 
-def test_type():
-    assert type('hello') is str
-    assert type(10) is int
+# def test_greater():
+#     assert 3<5
 
-def test_greater():
-    assert 3<5
+# def test_list():
+#     num_lis=[2,1,23,4,5]
+#     any_list= [False,False]
+#     assert 1 in num_lis
+#     assert 2 in num_lis
+#     assert all(num_lis)
+#     assert not any(any_list)
 
-def test_list():
-    num_lis=[2,1,23,4,5]
-    any_list= [False,False]
-    assert 1 in num_lis
-    assert 2 in num_lis
-    assert all(num_lis)
-    assert not any(any_list)
+
+class Student:
+    def __init__(self,first_name: str, last_name: str, major: str,years: int):
+        self.first_name=first_name
+        self.last_name=last_name
+        self.major= major
+        self.years=years
+        
+@pytest.fixture
+def default_employee():
+    return Student('john','doe','cs',3)
+
+def test_person_initilization(default_employee):
+    assert default_employee.first_name=='john','firstname should be john'
+    assert default_employee.last_name=='doe','lastname shouuld be doe'
+    assert default_employee.major=='cs'
+    assert default_employee.years==3
